d931c693860c488aa7e9cc79b3c8a61f7e9e79f5
cindywei <ying.wei@hand-china.com>
2019-09-19 01:20:21 +0800

    vue-router基础原理

---
+++ src/components/test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>this is a </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'A',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
